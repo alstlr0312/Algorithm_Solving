3354a8b18271dd19c7915cd632e89df3f59807b3
rlaalstlr <alstlrrla0312@gmail.com>
2022-05-18 15:09:46 +0900

    second_try_i have no error but answer is wrong....hummm..........

--- src/Programmers/Printer.java
+++ src/Programmers/Printer.java
@@ -12,16 +12,20 @@ public class Printer {
             int Big=queue.remove();                 //맨 앞에있는 프린터
             location--;                             //앞에 프린터가 remove되어서 queue안에 배열들이 한칸씩 땡겨졌으므로
                                                     //뽑아야하는 프린터의 위치도 한칸 앞으로 이동
+            boolean biggest = true;                 //우선순위가 가장 큰거 일시 true
             for(int p : queue){                     //queue 안에 배열들 우선순위 비교
-
                 if(Big<p){                          //맨 앞프린터보다 우선순위가 큰게 있을 경우
-                    queue.add(Big);
-                }
-                else{
-                    answer++;
+                    biggest = false;                //가장 우선순위가 높지 않으므로 false
+                    break;
                 }
             }
-            if(location==-1){
+            if(biggest = true){                     //프린터를 하나 출력했을 경우
+                answer++;                           //횟수 추가
+            }
+            else{                                   //만일 Big이 우선순위가 가장 높지 않았을 때
+                queue.add(Big);                     //맨뒤에 다시 삽입
+            }
+            if(location==-1){                      //구 할려는 프린터가 출력됬을때 반복문 정지
                 break;
             }
 
